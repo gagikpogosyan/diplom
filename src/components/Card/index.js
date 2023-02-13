b36fe75e71e89f01bf07c1d8353b9dc01aba4f0d
gagik <pogosyangagik680@gmail.com>
2023-02-13 19:41:14 +0300

    styles edit

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -33,7 +33,7 @@ function Card({
       {loading ? (
         <ContentLoader
           speed={2}
-          width={155}
+          width={180}
           height={250}
           viewBox="0 0 155 265"
           backgroundColor="#f3f3f3"
@@ -44,6 +44,7 @@ function Card({
           <rect x="0" y="187" rx="5" ry="5" width="100" height="15" />
           <rect x="1" y="234" rx="5" ry="5" width="80" height="25" />
           <rect x="124" y="230" rx="10" ry="10" width="32" height="32" />
+          <rect x="87" y="230" rx="10" ry="10" width="32" height="32" />
         </ContentLoader>
       ) : (
         <>
@@ -52,13 +53,13 @@ function Card({
               width={133}
               height={112}
               src={imageUrl}
-              alt="sneakers"
+              alt="flowers"
               className={styles.cardImg}
             />
           </div>
           <h5>{title}</h5>
-          <div className="d-flex justify-between align-center mt-10">
-            <div className="d-flex flex-column mr-5">
+          <div className="d-flex align-center mt-10">
+            <div className="d-flex flex-column mr-40">
               <span>Цена:</span>
               <b>{price} руб. /шт</b>
             </div>
